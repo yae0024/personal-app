fe6b63f6d4b481cbaaa9bb5771ad96ff80be5952
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-08-14 01:55:10 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_023549) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
