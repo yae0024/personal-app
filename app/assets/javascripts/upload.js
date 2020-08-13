1a40c14cb9aaf8ee7723f0b775a62960ae3468f3
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-08-14 01:52:35 +0900

    Set image preview display and tag input frame

--- app/assets/javascripts/upload.js
+++ app/assets/javascripts/upload.js
@@ -1,29 +1,30 @@
-// https://qiita.com/Masanori_N/items/71dbf648737f32dd8588
 $(function(){
   function buildHTML(image) {
     var html =
-      `<div class="prev-content">
-        <img src="${image}", alt="preview" class="prev-image">
-      </div>
-      <div>
-        <textarea name="tags_field" cols="40" rows="10" class="tag_field autosize pure-input-1 autosized" placeholder="タグ（コンマで区切って下さい）" style="height: 60px; overflow: hidden; overflow-wrap: break-word;" maxlength="1000" data-lang="ja" required=""></textarea>
-        <div>
-        <form class="button_to" method="post" action="/pictures"><input type="submit" value="保存" /></form>
-          <a>削除</a>
+      `<div class="attach-tags__content">
+        <div class="prev-image">
+          <img src="${image}", alt="preview" class="prev-image">
         </div>
-      </div>`
+        <div class="tags">
+          <input type="text" name="picture[name]" placeholder="タグ（スペース区切り）" class="tags__name">
+          <div class="tags__btn">
+            <input type="submit" name="submit" value="確認" class="tags__btn__submit" data-disable-with="Send">
+            <a class="tags__btn__delete">削除</a>
+          </div>
+        </div>
+      </div>
+      <hr>`
     return html;
   }
-  $(document).on('change', '#upload-img', function () {
-    console.log('OK');
+  $(document).on('change', '#attach-image', function () {
     var file = this.files[0];
     var reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function () {
       var image = this.result;
-      if ($('.prev-content').length == 0) {
+      if ($('.prev-image').length == 0) {
         var html = buildHTML(image)
-        $('.input-area').append(html);
+        $('.attach-tags').append(html);
       } 
     }
   });
