2acc28443474b8c2af882bef934104bd82f70fa1
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-08-14 02:08:33 +0900

    Write save_tags method

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,18 @@ class Picture < ApplicationRecord
   has_many :pictures_tags
   has_many :tag, through: :pictures_tags
   has_many :comments
+
+  def save_tags(tag_list)
+    tag_list.each do |tag|
+      unless find_tag = Tag.find_by(name: tag.downcase)
+        begin
+          self.tags.create!(name: tag)
+        rescue
+          nil
+        end
+      else
+        PictureTagRelation.create!(picture_id: self.id, tag_id: find_tag.id)
+      end
+    end
+  end
 end
