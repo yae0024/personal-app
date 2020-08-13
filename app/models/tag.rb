70e656ac03af7be8b1f87f8aa0bdc26600a9d4b3
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-08-14 01:58:49 +0900

    Unified tag names to downcase

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,10 @@
 class Tag < ApplicationRecord
+  before_save :downcase_tag_name
   has_many :pictures_tags, dependent: :destroy
   has_many :pictures, through: :pictures_tags
+
+  private
+    def downcase_tag_name
+      self.name.downcase!
+    end
 end
