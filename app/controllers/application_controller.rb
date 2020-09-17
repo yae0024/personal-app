25dad7bdd8451da7fcbfabf483f992deceac07e1
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-09-17 14:22:40 +0900

    Add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  private
+  def production?
+    Rails.env.production?
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   protected
   def configure_permitted_parameters
