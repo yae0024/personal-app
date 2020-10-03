6941238693d2a1fcff946b947d7bc1138cb8b6f6
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-10-03 14:45:39 +0900

    Set activeclass to user information tab

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -18,3 +18,16 @@ document.addEventListener("turbolinks:load", function(){
       $('#users-accordion').toggleClass('hidden');
     });
   });
+  // navbar activeclassの付け替え
+  if(document.URL.match("/media")) {
+    $(".my-media").addClass("active");
+  }else if(document.URL.match("/pictures/new")) {
+    $(".upload").addClass("active");
+  }else if(document.URL.match("/favorite")) {
+    $(".favorite").addClass("active");
+  }else if(document.URL.match("/message")) {
+    $(".message").addClass("active");
+  }else if(document.URL.match("/setting")) {
+    $(".setting").addClass("active");
+  }
+})
