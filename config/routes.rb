935c12751fd55fad1cc47a0a157bb19488b773eb
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-10-03 14:51:13 +0900

    Set routing for user information pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "welcome#index"
 
-  resources :pictures, except: [:index] do
+  resources :users do
+    member do
+      get 'media'
+      get 'favorite'
+      get 'message'
+      get 'setting'
+    end
+  end
+
+  resources :pictures do
     collection do
       get 'search'
     end
